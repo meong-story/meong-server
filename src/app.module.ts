5647010c087a08496c89071b183a67084fce33c7
Owon <chlehdnjs@gmail.com>
2024-04-14 15:21:33 +0900

    feat: user, pet CRUD generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PetModule } from './pet/pet.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PetModule],
   controllers: [AppController],
   providers: [AppService],
 })
