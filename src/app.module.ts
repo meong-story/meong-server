8f573f5fe12a23688497bdce5fe4573be7598392
Owon <chlehdnjs@gmail.com>
2024-04-19 01:49:02 +0900

    feat: db 연동, env 변수파일 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { PetModule } from './pet/pet.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pet } from './pet/entities/pet.entity';
+import { PetService } from './pet/pet.service';
+import { PetController } from './pet/pet.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PetModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Pet],
+      synchronize: true,
+      charset: 'utf8mb4',
+    }),
+    TypeOrmModule.forFeature([Pet]),
+  ],
+  providers: [PetService],
+  controllers: [PetController],
 })
 export class AppModule {}
